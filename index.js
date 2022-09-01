7f0d0fbb24334e484b79955ccf5ab95155d20043
Jay Harris <jay.harris@outlook.co.nz>
2022-09-01 15:41:53 +1200

    Fix styles in Svelte

--- index.js
+++ index.js
@@ -1,34 +1,40 @@
 const indent = (text, level) => text.split('\n')
-	.map(line => ''.padStart(level, '\t') + line)
-	.join('\n')
+    .map(line => ''.padStart(level, '\t') + line)
+    .join('\n')
 
-/**
- * @type {import('postcss').PluginCreator}
- */
-module.exports = (opts = {}) => {
-  // Work with options here
-
-  return {
-    postcssPlugin: 'darkmode',
-    AtRule: {
-      darkmode: atRule => {
-        const selectors = atRule.params.split(',').map(t => t.trim()).filter(s => s)
-        if (!selectors.length) selectors.push('&');
-
-        const body = atRule.nodes.join(';\n');
-        atRule.replaceWith(`
-@media (prefers-color-scheme: dark) {
-	${selectors.map(s => `${s}:not([data-theme="light"] ${s}):not([data-theme="light"])`).join(', ')} {
-${indent(body, 2)};
-	}
-}
-
-${selectors.map(s => `${s}[data-theme="dark"]`).join(', ')} {
-${indent(body, 1)}
-}`)
-      }
+const renderRule = (rule, transform) => {
+    if (!rule.selector) {
+        console.log(rule.toString())
+        return ''
     }
-  }
+    const selectors = rule.selector.split(',').map(s => s.trim()).map(transform)
+    return `${selectors.join(', ')} {
+${indent(rule.nodes.map(n => n.toString() + ';').join('\n'), 1)}
+}`
 }
 
-module.exports.postcss = true
+module.exports = (options = { forceGlobal: true }) => {
+    return {
+        postcssPlugin: 'darkmode',
+        AtRule: {
+            darkmode: atRule => {
+                // In Svelte, we need to force the selector to be global,
+                // otherwise the compiler will remove our style!
+                let parentDarkModeSelector = '[data-theme=dark]'
+                if (options.forceGlobal) parentDarkModeSelector = `:global(${parentDarkModeSelector})`
+                
+                const queryBody = atRule.nodes.map(n => renderRule(n, s => `${s}:not([data-theme=light] ${s}):not([data-theme=light])`)).join('\n\n');
+                const attributeBody = atRule.nodes.map(n => renderRule(n, s => `${s}[data-theme=dark], ${parentDarkModeSelector} ${s}`)).join('\n\n')
+
+                atRule.replaceWith(`
+@media (prefers-color-scheme: dark) {
+${indent(queryBody, 1)}
+}
+
+${attributeBody}`)
+            }
+        }
+    }
+}
+
+module.exports.postcss = true;
