4bc3a76a6697c2473d326627b0a6b8c7fed27864
Jay Harris <jay.harris@outlook.co.nz>
2022-09-07 10:35:45 +1200

    Update with new functionality

--- index.js
+++ index.js
@@ -13,18 +13,49 @@ ${indent(rule.nodes.map(n => n.toString() + ';').join('\n'), 1)}
 }`
 }
 
-module.exports = (options = { forceGlobal: true }) => {
+const generateNestedDarkModeSelectors = (levels, componentSelector, options) => {
+    if (levels === 0) return '';
+
+    const generateNestedDarkModeSelector = (level) => {
+        let selector = '[data-theme=dark]'
+        while (--level > 0)
+            selector = '[data-theme=dark] [data-theme=light] ' + selector;
+
+        // Mode can be undefined, global, or host-context. This let's us wrap
+        // the selector in different scenarios.
+        const getParentSelector = (s) => {
+            if (options.useHostContext) s = `:host-context(${s})`;
+            if (options.forceGlobal) s = `:global(${s})`;
+            return s;
+        }
+
+        // The :not needs to be wrapped in a :host-context if we should use the
+        // host context.
+        const getNotSelector = (s) => options.useHostContext ? `:host-context(${s})` : s;
+        return `${getParentSelector(selector)} ${componentSelector}:not(${getNotSelector(selector + ' [data-theme=light]')} ${componentSelector}):not([data-theme=light])`
+    }
+
+    return ',\n' + Array.from(Array(levels).keys()).map(i => generateNestedDarkModeSelector(i + 1))
+        .join(',\n')
+}
+
+const getNestingLevel = (params) => {
+    const result = /\(levels: (\d+)\)/g.exec(params);
+    if (result && result[1]) {
+        const parsed = parseInt(result[1]);
+        return isNaN(parsed) ? 2 : parsed;
+    }
+    return 2;
+}
+
+module.exports = (options = { forceGlobal: false, useHostContext: false }) => {
     return {
         postcssPlugin: 'darkmode',
         AtRule: {
             darkmode: atRule => {
-                // In Svelte, we need to force the selector to be global,
-                // otherwise the compiler will remove our style!
-                let parentDarkModeSelector = '[data-theme=dark]'
-                if (options.forceGlobal) parentDarkModeSelector = `:global(${parentDarkModeSelector})`
-                
-                const queryBody = atRule.nodes.map(n => renderRule(n, s => `${s}:not([data-theme=light] ${s}):not([data-theme=light])`)).join('\n\n');
-                const attributeBody = atRule.nodes.map(n => renderRule(n, s => `${s}[data-theme=dark], ${parentDarkModeSelector} ${s}`)).join('\n\n')
+                const nesting = getNestingLevel(atRule.params);
+                const queryBody = atRule.nodes.map(n => renderRule(n, s => `${s}:not([data-theme] ${s}):not([data-theme])`)).join('\n\n');
+                const attributeBody = atRule.nodes.map(n => renderRule(n, s => `${s}[data-theme=dark]${generateNestedDarkModeSelectors(nesting, s, options)}`)).join('\n\n')
 
                 atRule.replaceWith(`
 @media (prefers-color-scheme: dark) {
