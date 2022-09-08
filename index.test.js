030e4b612dab49045b8221bd1795cc38af50a7d8
Jay Harris <jay.harris@outlook.co.nz>
2022-09-08 13:19:05 +1200

    more work on tests

--- index.test.js
+++ index.test.js
@@ -4,7 +4,10 @@ const plugin = require('./')
 
 async function run (input, output, opts = { }) {
   let result = await postcss([plugin(opts)]).process(input, { from: undefined })
-  expect(result.css).toEqual(output)
+  const removeWhiteSpace = /(^\s+\n$)|(\t| )/g
+  const sansWhiteSpace = text => text.replace(removeWhiteSpace, '');
+
+  expect(sansWhiteSpace(result.toString())).toEqual(sansWhiteSpace(output))
   expect(result.warnings()).toHaveLength(0)
 }
 
@@ -12,79 +15,78 @@ async function run (input, output, opts = { }) {
 
 it('Converts the base case', async () => {
   await run(`.component {
-  padding: 12px;
-  background: pink;
-  display: flex;
-  flex-direction: row;
-}
-
-@darkmode {
-  .component {
-    background: red;
-    flex-direction: column;
+    padding: 12px;
+    background: pink;
+    display: flex;
+    flex-direction: row;
   }
-}`, `:root, [data-theme=light] {
-  --\\.component_background: pink;
-  --\\.component_flex-direction: row;
-}
 
-[data-theme=dark] {
-  --\\.component_background: red;
-  --\\.component_flex-direction: column;
-}
+  @darkmode {
+    .component {
+      background: red;
+      flex-direction: column;
+    }
+  }`, `:root, [data-theme=light] {
+    --\\.component_background: pink;
+    --\\.component_flex-direction: row;
+  }
 
-@media (prefers-color-scheme: dark) {
-
-  :root {
+  [data-theme=dark] {
     --\\.component_background: red;
     --\\.component_flex-direction: column;
   }
-}
 
-.component {
-  padding: 12px;
-  display: flex;
-}
-
-.component {
-  background: var(--\\.component_background);
-  flex-direction: var(--\\.component_flex-direction);
-}`, { })
-})
-
-test('dark mode only properties are converted', async () => {
-  await run(`.component {
-  background: pink;
-}
-
-@darkmode {
-  .component {
-    background: red;
-    color: white;
+  @media (prefers-color-scheme: dark) {
+    :root {
+      --\\.component_background: red;
+      --\\.component_flex-direction: column;
+    }
   }
-}`, `:root, [data-theme=light] {
-  --\\.component_background: pink;
-  --\\.component_color: unset;
-}
 
-[data-theme=dark] {
-  --\\.component_background: red;
-  --\\.component_color: white;
-}
+  .component {
+    padding: 12px;
+    display: flex;
+  }
 
-@media (prefers-color-scheme: dark) {
+  .component {
+    background: var(--\\.component_background);
+    flex-direction: var(--\\.component_flex-direction);
+  }`, { })
+  })
 
-  :root {
+  test('dark mode only properties are converted', async () => {
+    await run(`.component {
+    background: pink;
+  }
+
+  @darkmode {
+    .component {
+      background: red;
+      color: white;
+    }
+  }`, `:root, [data-theme=light] {
+    --\\.component_background: pink;
+    --\\.component_color: unset;
+  }
+
+  [data-theme=dark] {
     --\\.component_background: red;
     --\\.component_color: white;
   }
-}
 
-.component {
-}
+  @media (prefers-color-scheme: dark) {
+      :root {
+        --\\.component_background: red;
+        --\\.component_color: white;
+      }
+  }
 
-.component {
-  background: var(--\\.component_background);
-  color: var(--\\.component_color);
-}`, { })
+  .component {
+  }
+
+  .component {
+
+      background: var(--\\.component_background);
+      color: var(--\\.component_color);
+  }`, { })
 })
